41400e92d1922bfec72f42ceff1ac6c92eb67709
razvancir96 <razvan.cirlugea@gmail.com>
2021-09-26 23:37:00 +0300

    add forecast advanced version

--- sections/WeatherForecast.js
+++ sections/WeatherForecast.js
@@ -14,35 +14,54 @@ function displayWeatherForecast(city) {
       // Din datele venite, ne intereseaza doar list, care e un array.
       const { list } = data;
 
-      // Iteram prin list.
-      list.forEach((element) => {
-        // Pentru fiecare element, extragem datele de interes.
-        const { dt, main, weather } = element;
-        // getDayOfTheWeek si getHour sunt creata de noi, in utils/date.
-        const day = getDayOfTheWeek(dt);
-        const hour = getHour(dt);
-        // Rotunjim temperaturile.
-        const temperature = Math.round(main.temp);
-        const realFeel = Math.round(main.feels_like);
-        // Atentie! weather este un array, cu un singur element.
-        const weatherDescription = weather[0].description;
-        // getWeatherIcon este creata de noi, in utils/weather.
-        const weatherIcon = getWeatherIcon(weather[0].icon);
+      // Avem nevoie de un obiect in care sa grupam predictiile pe zile:
+      const daysMap = {};
 
-        // Afisam pe ecran informatiile extrase din API.
-        weatherForecastContainer.innerHTML += `
-          <div class="weather-forecast-box w-100 d-flex justify-content-between align-items-center">
-            <div>
-              <div><strong>${day}</strong></div>
-              <div>${hour}</div>
-            </div>
-            <div><img src="${weatherIcon}" alt="" /></div>
-            <div><strong>${temperature}°</strong></div>
-            <div>${weatherDescription}</div>
-            <div>Real feel: ${realFeel}</div>
-          </div>
-        `;
+      // Iteram prin cele 40 de predictii primite de la server.
+      list.forEach((element) => {
+        // Extragem data predictiei.
+        const { dt } = element;
+        // getDayOfTheWeek este creata de noi, in utils/date.
+        const day = getDayOfTheWeek(dt);
+        // Daca deja avem ziua saptamanii in obiect, ii aduagam o noua predictie.
+        if (daysMap[day]) {
+          daysMap[day].push(element);
+          // Altfel, adaugam ziua saptamanii in obiect, alaturi de noua predictie.
+        } else {
+          daysMap[day] = [element];
+        }
       });
 
+      // Parcurgem cu for...in continutul obiectului daysMap. Cheile sunt zilele saptamanii pentru care avem predictii.
+      for (key in daysMap) {
+        // Afisam ziua saptamanii pe ecran.
+        weatherForecastContainer.innerHTML += `<h3 class="text-primary">${key}</h3>`;
+        // Pentru fiecare zi a saptamanii, extragem predictiile asociate si iteram prin ele.
+        days = daysMap[key];
+        days.forEach((element) => {
+          // Pentru fiecare element (predictie), extragem datele de interes.
+          const { dt, main, weather } = element;
+          // getHour este creata de noi, in utils/date.
+          const hour = getHour(dt);
+          // Rotunjim temperaturile.
+          const temperature = Math.round(main.temp);
+          const realFeel = Math.round(main.feels_like);
+          // Atentie! weather este un array, cu un singur element.
+          const weatherDescription = weather[0].description;
+          // getWeatherIcon este creata de noi, in utils/weather.
+          const weatherIcon = getWeatherIcon(weather[0].icon);
+
+          // Afisam pe ecran informatiile extrase din API.
+          weatherForecastContainer.innerHTML += `
+            <div class="weather-forecast-box w-100 d-flex justify-content-between align-items-center border rounded p-3 mb-3">
+              <div>${hour}</div>
+              <div><img src="${weatherIcon}" alt="" /></div>
+              <div class="fs-3"><strong>${temperature}°C</strong></div>
+              <div>${weatherDescription}</div>
+              <div class="real-feel">Real feel: <strong>${realFeel}°C</strong></div>
+            </div>
+          `;
+        });
+      }
     });
 }
