4f112f8cdf98aa676c6f44bbe61f8053ae4542ea
razvancir96 <razvan.cirlugea@gmail.com>
2021-09-26 22:33:46 +0300

    extract API data and display it
    add missing styles

--- sections/CurrentWeather.js
+++ sections/CurrentWeather.js
@@ -7,5 +7,36 @@ function displayCurrentWeather() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      // Extragem proprietatile care ne intereseaza.
+      const { name, dt, main, weather, wind } = data;
+      // getDayOfTheWeek si getHour sunt creata de noi, in utils/date.
+      const day = getDayOfTheWeek(dt);
+      const hours = getHour(dt);
+      // Rotunjim temperaturile.
+      const temperature = Math.round(main.temp);
+      const realFeel = Math.round(main.feels_like);
+      // Atentie! weather este un array, cu un singur element.
+      const weatherDescription = weather[0].description;
+      // getWeatherIcon si windToKmPerHour sunt create de noi, in utils/weather.
+      const weatherIcon = getWeatherIcon(weather[0].icon);
+      const windSpeed = Math.round(windToKmPerHour(wind.speed));
+
+      // Afisam pe ecran informatiile extrase din API.
+      let currentWeatherContainer = document.querySelector(".current-weather");
+      currentWeatherContainer.innerHTML = `
+        <div class="px-3">
+          <div class="fs-2 mb-2"><strong>${name}</strong></div>
+          <div class="fs-4"><strong>${day}</strong>, ${hours}</div>
+          <div class="d-flex align-items-center justify-content-center">
+            <strong class="fs-1">${temperature}°C</strong>
+            <img src="${weatherIcon}" />
+          </div>
+        </div>
+        <div class="px-3">
+          <p class="fs-5">Real feel: <strong>${realFeel}°C</strong></p>
+          <p class="fs-5 text-capitalize">${weatherDescription}</p>
+          <p class="fs-5">Vânt: <strong>${windSpeed} km/h</strong></p>
+        </div>
+      `;
     });
 }
